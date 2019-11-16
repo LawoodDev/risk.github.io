11ea52faa2fcf244340be60eb281b5d84cf96ed7
Erwann <erwann.sale96@gmail.com>
2019-11-16 23:02:08 +0100

    fix part of progressive

--- sw.js
+++ sw.js
@@ -19,6 +19,7 @@ var filesToCache = [
     '/index.html',
     '/images.html',
     '/style.css',
+    '/manifest.json',
     '/ressources/js/main.js'
 ];
 /* Start the service worker and cache all of the app's content */
