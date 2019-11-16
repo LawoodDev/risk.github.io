a4c5abe8f3d353a76e2d825ee0512aaeab21d2a4
Erwann <erwann.sale96@gmail.com>
2019-11-16 22:36:18 +0100

    fix promise on sw.js

--- ressources/js/sw.js
+++ ressources/js/sw.js
@@ -1,25 +1,25 @@
-var cacheName = 'Claris Design';
+var cacheName = 'claris-design';
 var filesToCache = [
-    '/portfolio/',
+    './',
     './ressources/font/Roboto-Light.ttf',
     './ressources/images/arrow.png',
     './ressources/images/bg.png',
     './ressources/images/bouton-menu-n.png',
     './ressources/images/bouton-menu.png',
     './ressources/images/logo-n.png',
-    '/portfolio/ressources/images/logoCD.png',
-    '/portfolio/ressources/images/menu-apropos-n.png',
-    '/portfolio/ressources/images/menu-apropos.png',
-    '/portfolio/ressources/images/menu-contact-n.png',
-    '/portfolio/ressources/images/menu-contact.png',
-    '/portfolio/ressources/images/menu-interfaces-n.png',
-    '/portfolio/ressources/images/menu-interfaces.png',
-    '/portfolio/ressources/images/menu-logo-n.png',
-    '/portfolio/ressources/images/menu-logo.png',
-    '/portfolio/index.html',
-    '/portfolio/images.html',
-    '/portfolio/style.css',
-    '/portfolio/main.js'
+    './portfolio/ressources/images/logoCD.png',
+    './portfolio/ressources/images/menu-apropos-n.png',
+    './portfolio/ressources/images/menu-apropos.png',
+    './portfolio/ressources/images/menu-contact-n.png',
+    './portfolio/ressources/images/menu-contact.png',
+    './portfolio/ressources/images/menu-interfaces-n.png',
+    './portfolio/ressources/images/menu-interfaces.png',
+    './portfolio/ressources/images/menu-logo-n.png',
+    './portfolio/ressources/images/menu-logo.png',
+    './portfolio/index.html',
+    './portfolio/images.html',
+    './portfolio/style.css',
+    './portfolio/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
@@ -27,7 +27,7 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             return cache.addAll(filesToCache);
-        })
+        }).catch(console.error)
     );
 });
 
@@ -36,6 +36,6 @@ self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (response) {
             return response || fetch(e.request);
-        })
+        }).catch(console.error)
     );
 });
