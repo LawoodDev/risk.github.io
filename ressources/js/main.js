c9cac33beeb95d75f2c9bb10ce49e86f62d82d0f
Erwann <erwann.sale96@gmail.com>
2019-11-16 22:18:44 +0100

    First commit

---
+++ ressources/js/main.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    'use strict';
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./ressources/js/sw.js');
+    }
+    var buttonImg = document.getElementById("buttonImg");
+    buttonImg.onclick = () => {
+        var element = document.getElementById("button");
+        element.classList.toggle("slide");
+
+
+        var element1 = document.getElementsByClassName("categorie");
+        for (var r of element1)
+            r.classList.toggle("show");
+
+    }
+
+}
+
+function but() {
+
+}
---
+++ ressources/js/sw.js
@@ -0,0 +1,41 @@
+var cacheName = 'Claris Design';
+var filesToCache = [
+    '/portfolio/',
+    './ressources/font/Roboto-Light.ttf',
+    './ressources/images/arrow.png',
+    './ressources/images/bg.png',
+    './ressources/images/bouton-menu-n.png',
+    './ressources/images/bouton-menu.png',
+    './ressources/images/logo-n.png',
+    '/portfolio/ressources/images/logoCD.png',
+    '/portfolio/ressources/images/menu-apropos-n.png',
+    '/portfolio/ressources/images/menu-apropos.png',
+    '/portfolio/ressources/images/menu-contact-n.png',
+    '/portfolio/ressources/images/menu-contact.png',
+    '/portfolio/ressources/images/menu-interfaces-n.png',
+    '/portfolio/ressources/images/menu-interfaces.png',
+    '/portfolio/ressources/images/menu-logo-n.png',
+    '/portfolio/ressources/images/menu-logo.png',
+    '/portfolio/index.html',
+    '/portfolio/images.html',
+    '/portfolio/style.css',
+    '/portfolio/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
